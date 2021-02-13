a9c5ca0cddeb36885be0e7b97c66a4ca8beac7b4
Rajput, Jitendra <jrajput@visa.com>
2021-02-13 10:10:14 +0530

    Learning project is addex

---
+++ java/Learning/src/main/java/com/test/LearningApp.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApp {
+
+  public static void main(String[] args) {
+    new SpringApplication(LearningApp.class).run();
+  }
+}
---
+++ java/Learning/src/test/java/com/test/AddTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+public class AddTest {
+
+  @Test
+  public void testAdd(){
+    int a=2;
+    int b=3;
+    int sum =a+b;
+    System.out.println(sum);
+  }
+}
